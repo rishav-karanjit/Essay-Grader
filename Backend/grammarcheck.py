ea547dca91326369260fd0aa4fb815ba0bc03fad
Rishav Karanjit <karanjitrishav4@gmail.com>
2020-11-26 20:12:53 +0545

    Updated grammercheck

--- Backend/grammarcheck.py
+++ Backend/grammarcheck.py
@@ -1,42 +1,46 @@
 import language_tool_python
 import os
-tool = language_tool_python.LanguageTool('en-US')
- 
-f = open("essay.txt", "r")
-text = f.read()
- 
-# get the matches
-matches = tool.check(text)
-print("No. of grammatical mistake:",len(matches),end="\n\n")
-print("------------------------------------------")
-translation_table = dict.fromkeys(map(ord, '^'), None)
 
-printable = 0
-for i in matches:
-    i = str(i)
+class gcheck():
+    def Check_Grammer(self):
+        tool = language_tool_python.LanguageTool('en-US')
+         
+        f = open("backend/essay.txt", "r")
+        fw = open("backend/grammer_mistake.txt","w")
+        text = f.read()
+         
+        # get the matches
+        matches = tool.check(text)
+        fw.write("No. of grammatical mistake:"+str(len(matches))+"\n\n")
+        fw.write("------------------------------------------\n")
+        translation_table = dict.fromkeys(map(ord, '^'), None)
 
-    #Get offset+length
-    offset = i.split(',')[0]
-    length = i.split(',')[1]
+        printable = 0
+        for i in matches:
+            i = str(i)
 
-    length = int(length.replace("length ",""))
-    offset = int(offset.replace("Offset ",""))
-    
-    #Get range to print
-    previousprint = printable
-    printable = length + offset
+            #Get offset+length
+            offset = i.split(',')[0]
+            length = i.split(',')[1]
 
-    print(text[previousprint:printable],end="")
+            length = int(length.replace("length ",""))
+            offset = int(offset.replace("Offset ",""))
+            
+            #Get range to print
+            previousprint = printable
+            printable = length + offset
 
-    tillfullstop = text[printable:].split('.')[0] + "."
-    printable += len(tillfullstop)
+            fw.write(text[previousprint:printable])
 
-    print(tillfullstop,end="\n\n")
-    #Remove ^
-    i = i.translate(translation_table)
+            tillfullstop = text[printable:].split('.')[0] + "."
+            printable += len(tillfullstop)
 
-    #split each line to list and remove last element. Then print.
-    i = i.splitlines(True)[:-1]
-    for j in i:
-        print(j,end="")
-    print("------------------------------------------")
+            fw.write(tillfullstop+"\n\n")
+            #Remove ^
+            i = i.translate(translation_table)
+
+            #split each line to list and remove last element. Then print.
+            i = i.splitlines(True)[:-1]
+            for j in i:
+                fw.write(j)
+            fw.write("------------------------------------------\n")
