ea73a2280d2a3826cfa3a34e57f47b6746b23b51
Rishav Karanjit <karanjitrishav4@gmail.com>
2020-11-26 20:12:10 +0545

    Updated GrammerCheck UI

--- Grammer_checkUI.py
+++ Grammer_checkUI.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets, uic
 import sys
-
+from Backend.grammarcheck import gcheck
 class Mistakes(QtWidgets.QMainWindow):
     def __init__(self):
         super(Mistakes, self).__init__()
@@ -8,3 +8,14 @@ class Mistakes(QtWidgets.QMainWindow):
 
     def initUI(self): 
         uic.loadUi('./UI/Mistakes.ui', self)
+        grammer = gcheck()
+        grammer.Check_Grammer()
+
+        f = open("backend/grammer_mistake.txt", "r")
+        self.gmistake_textedit = self.findChild(QtWidgets.QPlainTextEdit, 'gmistake')
+        self.gmistake_textedit.insertPlainText(f.read())
+
+        f = open("backend/essay.txt", "r")
+        self.essay_textedit = self.findChild(QtWidgets.QPlainTextEdit, 'essay')
+        self.essay_textedit.insertPlainText(f.read())
+        
