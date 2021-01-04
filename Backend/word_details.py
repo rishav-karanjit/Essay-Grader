9714d731b23c4973c23feea70014bd77674be95e
Rishav Karanjit <karanjitrishav4@gmail.com>
2021-01-04 17:16:13 +0545

    Updated words detail

--- Backend/word_details.py
+++ Backend/word_details.py
@@ -1,20 +1,20 @@
-import nltk
+import nltk, re
 class WordDetails():    
     def Get_No_of_char(self):
-        file = open("essay.txt", "r")
+        file = open("Backend/essay.txt", "r")
         data = file.read().replace(" ","")
         number_of_characters = len(data)
-        return(number_of_characters)
+        return(str(number_of_characters))
 
     def Get_No_of_words(self):
-        file = open("essay.txt", "r")
+        file = open("Backend/essay.txt", "r")
         data = file.read()
         words = data.split()
-        return(len(words))
+        return(str(len(words)))
 
     def Get_No_of_unique_words(self):
-        file = open("essay.txt", "r")
-        lines = file.readlines()
+        file = open("Backend/essay.txt", "r")
+        lines = file.read()
         words = []
         unique = []
         for i in lines:
@@ -25,23 +25,34 @@ class WordDetails():
         count = 0
         for k in unique:
             count+=1
-        return(count)
+        return(str(count))
 
     def GetMostCommonWords(self):
-        file = open("essay.txt", "r")
-        data = file.read().replace('\n', ' ')
+        file = open("Backend/essay.txt", "r")
+        data = file.read()
+        data = re.sub(r'[^\w\s]', '', data) 
+        data_tokens = nltk.word_tokenize(data)
 
-        data = data.split(' ')
-        data = [x for x in data if x != '']
-        fdist1 = nltk.FreqDist(data)
-        a=[]
-        for i in fdist1.most_common(5):
-            a+=[i[0]]
+        stop_words = set(nltk.corpus.stopwords.words('english'))  
+        filtered_sentence = [w for w in data_tokens if not w in stop_words]        
+        filtered_sentence = []  
+        for w in data_tokens:  
+            if w not in stop_words:  
+                filtered_sentence.append(w)  
+        freq = nltk.FreqDist(filtered_sentence)
+        print(freq.most_common(5))
+        word = ""
+        for i in freq.most_common(5):
+            word += i[0]
+            word += "\n"
+        return(word)
+    def Get_No_of_para(self):
+        file = open("Backend/essay.txt", "r")
+        corpusReader = nltk.corpus.reader.plaintext.PlaintextCorpusReader("Backend", "essay.txt")
+        return(str(len(corpusReader.paras())))
 
-        return a
-
-Word = WordDetails()
-print(Word.Get_No_of_char())
-print(Word.Get_No_of_words())
-print(Word.Get_No_of_unique_words())
-print(Word.GetMostCommonWords())
+# Word = WordDetails()
+# print(Word.Get_No_of_char())
+# print(Word.Get_No_of_words())
+# print(Word.Get_No_of_unique_words())
+# print(Word.GetMostCommonWords())
--- DetailsUI.py
+++ DetailsUI.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
+from Backend.word_details import WordDetails
 class DetailsUI(QtWidgets.QMainWindow):
     def __init__(self):
         super(DetailsUI, self).__init__()
@@ -8,3 +9,25 @@ class DetailsUI(QtWidgets.QMainWindow):
 
     def initUI(self): 
         uic.loadUi('./UI/Word_Details.ui', self) 
+        
+        f = open("backend/essay.txt", "r")
+        self.essay_textedit = self.findChild(QtWidgets.QPlainTextEdit, 'essay')
+        self.essay_textedit.setReadOnly(True)
+        self.essay_textedit.insertPlainText(f.read())
+
+        self.WordDetails = WordDetails()
+
+        self.No_of_chars = self.findChild(QtWidgets.QLabel,'No_of_chars')
+        self.No_of_chars.setText(WordDetails.Get_No_of_char(self))
+
+        self.No_of_words = self.findChild(QtWidgets.QLabel,'No_of_words')
+        self.No_of_words.setText(WordDetails.Get_No_of_words(self))
+
+        self.No_of_paragraph = self.findChild(QtWidgets.QLabel,'No_of_paragraph')
+        self.No_of_paragraph.setText(WordDetails.Get_No_of_para(self))
+
+        self.No_of_unique = self.findChild(QtWidgets.QLabel,'No_of_unique')
+        self.No_of_unique.setText(WordDetails.Get_No_of_unique_words(self))
+
+        self.No_of_common = self.findChild(QtWidgets.QLabel,'No_of_common')
+        self.No_of_common.setText(WordDetails.GetMostCommonWords(self))
