5bf5e3265ccbf42bb4f2ced7c044415ea874da57
Rishav Karanjit <karanjitrishav4@gmail.com>
2021-05-09 17:06:02 +0545

    Added part of speech

--- Backend/word_details.py
+++ Backend/word_details.py
@@ -1,4 +1,7 @@
 import nltk, re
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+
 class WordDetails():    
     def Get_No_of_char(self):
         file = open("Backend/essay.txt", "r")
@@ -50,8 +53,57 @@ class WordDetails():
         corpusReader = nltk.corpus.reader.plaintext.PlaintextCorpusReader("Backend", "essay.txt")
         return(str(len(corpusReader.paras())))
 
+    # Tokenize a sentence into words
+
+    def sentence_to_wordlist(self,raw_sentence):
+        
+        clean_sentence = re.sub("[^a-zA-Z0-9]"," ", raw_sentence)
+        tokens = nltk.word_tokenize(clean_sentence)
+        
+        return tokens
+
+    def tokenize(self,essay):
+        stripped_essay = essay.strip()
+        
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+        raw_sentences = tokenizer.tokenize(stripped_essay)
+        
+        tokenized_sentences = []
+        for raw_sentence in raw_sentences:
+            if len(raw_sentence) > 0:
+                tokenized_sentences.append(self.sentence_to_wordlist(raw_sentence))
+        
+        return tokenized_sentences
+    def Part_of_speech(self):
+        file = open("Backend/essay.txt", "r")
+        essay = file.read()
+        tokenized_sentences = self.tokenize(essay)
+        
+        noun_count = 0
+        adj_count = 0
+        verb_count = 0
+        adv_count = 0
+        
+        for sentence in tokenized_sentences:
+            tagged_tokens = nltk.pos_tag(sentence)
+            
+            for token_tuple in tagged_tokens:
+                pos_tag = token_tuple[1]
+            
+                if pos_tag.startswith('N'): 
+                    noun_count += 1
+                elif pos_tag.startswith('J'):
+                    adj_count += 1
+                elif pos_tag.startswith('V'):
+                    verb_count += 1
+                elif pos_tag.startswith('R'):
+                    adv_count += 1
+                
+        return noun_count, adj_count, verb_count, adv_count
+        
 # Word = WordDetails()
 # print(Word.Get_No_of_char())
 # print(Word.Get_No_of_words())
 # print(Word.Get_No_of_unique_words())
 # print(Word.GetMostCommonWords())
+# print(Word.Get_No_of_Noun())
