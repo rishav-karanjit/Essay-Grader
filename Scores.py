629aaf08d4854b1d3ddf03ec86878243eba94384
Rishav Karanjit <karanjitrishav4@gmail.com>
2021-06-16 15:16:41 +0545

    Updated Scores

--- Scores.py
+++ Scores.py
@@ -1,14 +1,29 @@
 from PyQt5 import QtWidgets, uic
 import sys
 from Model.Loadmodel import Load_model
-class ScoreUI(QtWidgets.QMainWindow):
-    def __init__(self,main):
-        super(ScoreUI, self).__init__()
-        self.load_model = Load_model()
-        self.initUI()
-        self.show()
+from PyQt5 import QtWidgets, uic, QtCore
 
-    def initUI(self): 
-        uic.loadUi('./UI/Score_2.ui', self)
-        score = self.load_model.Load_StackModel()
-        self.findChild(QtWidgets.QLabel, 'Score').setText(str(score[0]))
+class ScoreUI(QtWidgets.QMainWindow):
+	def __init__(self,main):
+		self.main = main
+		super(ScoreUI, self).__init__()
+		self.load_model = Load_model()
+		self.initUI()
+		self.show()
+		self.main.close()
+
+	def initUI(self):
+		uic.loadUi('./UI/Score_2.ui', self)
+
+		self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+		score = self.load_model.Load_StackModel()
+		self.findChild(QtWidgets.QLabel, 'Score').setText(str(score[0]))
+		self.Close = self.findChild(QtWidgets.QPushButton, 'Close')
+		self.minimize = self.findChild(QtWidgets.QPushButton, 'minimize')
+		self.Close.clicked.connect(self.Close_and_Open)
+		self.minimize.clicked.connect(self.showMinimized)
+
+	def Close_and_Open(self):
+		self.main.show()
+		self.close()
